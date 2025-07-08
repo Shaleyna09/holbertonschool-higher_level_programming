f9859ef8b4017e6b5e832596d275e733a3cdf715
Shaleyna09 <shaleynarungasamy@gmail.com>
2025-07-08 11:23:19 +0400

    Task8

---
+++ python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,8 @@
+def uppercase(str):
+    result = ""
+    for c in str:
+        if ord('a') <= ord(c) <= ord('z'):
+            result += chr(ord(c) - 32)  # Convert to uppercase
+        else:
+            result += c
+    print('"{}'.format(result))
