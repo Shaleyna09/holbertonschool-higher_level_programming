38e734a006f3b1f8c232ef7f01842a205a7e9416
Shaleyna09 <shaleynarungasamy@gmail.com>
2025-07-12 09:45:54 +0400

    xderfvbhj

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
+my_list = [1, 2, 3, 4, 5]
+idx = 3
+new_element = 9
 
 def replace_in_list(my_list, idx, element):
     if idx < 0 or idx >= len(my_list):
         return my_list
-    my_list[idx] = element
+    my_list[idx] = new_element
     return my_list
