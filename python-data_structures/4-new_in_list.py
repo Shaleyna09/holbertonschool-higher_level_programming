8128adac154eb3e5ed2b7c3cfbdde5454225c589
Shaleyna09 <shaleynarungasamy@gmail.com>
2025-07-12 09:48:47 +0400

    frj

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
-my_list = [1, 2, 3, 4, 5]
-idx = 3
-new_element = 9
+
 
 def replace_in_list(my_list, idx, element):
     if idx < 0 or idx >= len(my_list):
-        return my_list
-    my_list[idx] = new_element
-    return my_list
+        return my_list[:]
+    new_list = my_list[:]
+    my_list[idx] = element
+    return new_list
